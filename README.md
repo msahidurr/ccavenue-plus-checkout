64536ec90d9557b4b24a9c1f9ff211d027388258
ringlives <shohidurr49@gmail.com>
2024-03-13 14:14:10 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# CCAvenuePlus Checkout
+
+With a simple, single integration you can accept debit and credit cards, PayPal, and 10+ local payment methods.
+
+## Install the Gateway
+
+1. You can install the gateway via composer:
+
+    ```
+    composer require blesta/ccavenue_plus
+    ```
+
+2. OR upload the source code to a /components/gateways/nonmerchant/ccavenue_plus/ directory within
+your Blesta installation path.
+
+    For example:
+
+    ```
+    /var/www/html/blesta/components/gateways/nonmerchant/ccavenue_plus/
+    ```
+
+3. Log in to your admin Blesta account and navigate to
+> Settings > Payment Gateways
+
+4. Find the CCAvenue Checkout gateway and click the "Install" button to install it
+
+5. You're done!
---
+++ language/en_us/ccavenue_plus.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * en_us language for the CCAvenuePlus Checkout gateway.
+ */
+// Basics
+$lang['CCAvenuePlus.name'] = 'CCAvenue Checkout';
+$lang['CCAvenuePlus.description'] = 'With a simple, single integration you can accept debit and credit cards, CCAvenuePlus, and 10+ local payment methods.';
+
+
+// Errors
+$lang['CCAvenue_Plus.!error.client_id.valid'] = 'Client ID invalid.';
+$lang['CCAvenue_Plus.!error.client_secret.valid'] = 'Client Secret invalid.';
+$lang['CCAvenue_Plus.!error.event.unsupported'] = 'Unsupported event type.';
+$lang['CCAvenue_Plus.!error.transaction.missing'] = 'No transaction found.';
+
+
+// Settings
+$lang['CCAvenue_Plus.meta.client_id'] = 'Client ID';
+$lang['CCAvenue_Plus.meta.client_secret'] = 'Client Secret';
+$lang['CCAvenue_Plus.meta.sandbox'] = 'Sandbox';
+
+$lang['CCAvenue_Plus.webhook'] = 'CCAvenuePlus Webhook';
+$lang['CCAvenue_Plus.webhook_note'] = 'Before you start using this gateway you must configure the following url as a Webhook in your CCAvenuePlus account.';
